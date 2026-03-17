f4ef5696f2ee858444a2bc856916e422133e2842
Terry Tao <librakevin@gmail.com>
2026-03-17 02:24:39 +0000

    update scripts/setup_runpod.sh

--- scripts/setup_runpod.sh
+++ scripts/setup_runpod.sh
@@ -1,8 +1,14 @@
 #! /usr/bin/env bash
 
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
 # Install linux packages
 apt-get update
 apt-get install -y less python3-dev build-essential ranger tmux unzip vim
+
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 
 # git
 git config --global user.email "librakevin@gmail.com"
